5a0567575306d2796d5e8748ecd7858abce32316
Laura <saleslaura20@gmail.com>
2025-10-28 18:45:54 +0100

    Initial commit - setup iOS project

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Laura Sales Martínez on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
